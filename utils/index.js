af7558d47213f88c341108960baa476c83579c16
Federico Knüssel <fknussel@gmail.com>
2017-02-11 15:04:30 -0300

    Add build utils

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,6 @@
-import fs from 'fs-extra';
 import chalk from 'chalk';
+import fs from 'fs-extra';
+import path from 'path';
 import paths from '../config/paths';
 
 // Log a green success message to the terminal
@@ -23,4 +24,58 @@ export function copyPublicFolder() {
 export function createBuildDirectory() {
   fs.removeSync(paths.build);
   fs.mkdirpSync(paths.build);
+}
+
+// Resolve relative paths.
+const appDirectory = fs.realpathSync(process.cwd());
+
+export function resolveApp(relativePath) {
+  return path.resolve(appDirectory, relativePath);
+}
+
+// Warn and crash if required files are missing.
+export function checkRequiredFiles() {
+  const indexHtmlExists = fs.existsSync(config.paths.index.html);
+  const indexJsExists = fs.existsSync(config.paths.index.js);
+
+  if (!indexHtmlExists || !indexJsExists) {
+    logError(`\nSome required files couldn't be found. Make sure these all exist:\n`);
+    logError(`\t- ${config.paths.index.html}`);
+    logError(`\t- ${config.paths.index.js}\n`);
+
+    process.exit(1);
+  }
+}
+
+// Print a detailed summary of build files.
+export function printFileSizes(stats) {
+  stats.toJson().assets
+    .filter(asset => /\.(js|css)$/.test(asset.name))
+    .map(asset => {
+      const fileStats = fs.statSync('./build/' + asset.name);
+      const fileSize = (fileStats.size / 1000).toFixed(2); // in kbs, originally in bytes
+
+      return {
+        name: asset.name,
+        size: fileSize,
+        unit: 'Kb.'
+      };
+    })
+    .sort((a, b) => b.size - a.size)
+    .forEach(asset => {
+      const {name, size, unit} = asset;
+
+      logSuccess(`\t- ${name} -> ${size} ${unit}`);
+    });
+}
+
+// Print out a list of errors to the terminal.
+export function printErrors(summary, errors) {
+  logError(`${summary}\n`);
+
+  errors.forEach(err => {
+    logError(`\t- ${err.message || err}`);
+  });
+
+  console.log();
 }
