927c2ecc5b98aef31579ee2bdffd79ec7282bedc
Federico Knüssel <fknussel@gmail.com>
2017-02-11 15:15:54 -0300

    Add paths configuration

---
+++ config/paths.js
@@ -0,0 +1,18 @@
+import {resolveApp} from '../utils';
+
+export default {
+  // The source folder is where the application source code lives in.
+  source: resolveApp('src'),
+
+  // The public folder containing the index.html, favicon.ico and other static assets.
+  public: resolveApp('public'),
+
+  // The build folder where we'll copy the bundled files.
+  build: resolveApp('build'),
+
+  // custom-scripts assumes this file to be the and therefore it must exist
+  indexHtml: resolveApp('public/index.html'),
+
+  // custom-scripts assumes this file to be the application entry point and therefore it must exist.
+  indexJs: resolveApp('src/index.js')
+};
