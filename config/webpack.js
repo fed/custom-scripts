8efad364e145bb0299c7c2c162c71d7e6abf4111
Federico Knüssel <fknussel@gmail.com>
2017-02-13 19:23:16 -0300

    Watch files for changes and refresh the page accordingly

--- config/webpack.js
+++ config/webpack.js
@@ -25,11 +25,20 @@ function uglify() {
 }
 
 module.exports = {
-  entry: paths.indexJs,
+  entry: [
+    // Include an alternative client for WebpackDevServer. A client's job is to
+    // connect to WebpackDevServer by a socket and get notified about changes.
+    // When you save a file, the client will either apply hot updates (in case
+    // of CSS changes), or refresh the page (in case of JS changes). When you
+    // make a syntax error, this client will display a syntax error overlay.
+    require.resolve('react-dev-utils/webpackHotDevClient'),
+
+    // The entry point to our app.
+    paths.indexJs
+  ],
 
   output: {
     // Needs to be an absolute path
-    // path: resolve(__dirname, 'build'),
     path: paths.build,
 
     filename: 'bundle.js',
